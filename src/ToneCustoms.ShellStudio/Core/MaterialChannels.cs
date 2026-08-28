a1352e1886e7cdbb83924d705d6d52c89f93a416
Tony Santiago <114844357+tonyiswavy@users.noreply.github.com>
2026-08-28 18:25:49 -0400

    Add advanced material channel model

---
+++ src/ToneCustoms.ShellStudio/Core/MaterialChannels.cs
@@ -0,0 +1,2 @@
+namespace ToneCustoms.ShellStudio.Core;
+public sealed class MaterialChannels { public string? RoughnessSource {get;set;} public string? MetallicSource {get;set;} public string? AmbientOcclusionSource {get;set;} public float Rotation {get;set;} public float OffsetU {get;set;} public float OffsetV {get;set;} }
